e28e990fb5d7f939096eb9ac148fac98d1084ce1
menezes <bielff070@gmail.com>
2026-03-20 09:42:18 -0300

    Subindo Arquivos do dia 20/03

---
+++ README.md
@@ -0,0 +1,26 @@
+# ANOTAÇÕES DE FUNDAMENTOS DA PROGRAMAÇÃO
+
+## Tipos de dados em python
+1. string 
+2. number int
+3. number float 
+4. boolean
+
+## Operadores matemáticos - básicos
+
++ -> adição
+- -> subtração
+* -> multiplicação 
+/ - > divisao 
+
+## Métodos em python
+
+1. print() -> Exibe informações no terminal.
+
+
+## Format em python
+
+# Estrutura de Repetição
+ ``if (se)`` -> Verifica se uma informação é true. Se for ele executa o código 
+ ``elif (senão se)`` -> É usado para testar várias condições. Ele só executa o código
+ ``else (senão)`` ->Executa o código se a condiçõo if for false 
---
+++ aula_08/estruturas.py
@@ -0,0 +1,22 @@
+# if (se) -> Verifica se uma informação é true. Se for ele executa o código 
+# elif (senão se) -> É usado para testar várias condições. Ele só executa o código
+# else(senão) -> Executa o código se a condiçõo if for false 
+
+# idade_usuario = 15   
+# # se o usuario for maior de 18 anos , eu vou passar a informacao;vc é maior de idade , senao vc e menor de idade 
+
+
+# if idade_usuario >= 18:
+#     print("voce e maior de idade")
+# else:
+#     print("voce e menor de idade")
+
+idade = 73
+if idade >= 18:
+    print("voce e maior de idade")
+elif idade >= 0 and idade < 18:
+    print("voce e jovem de idade")
+elif idade >= 70:
+    print("voce e experiente de idade")
+else:
+     print("voce e menor de idade")
